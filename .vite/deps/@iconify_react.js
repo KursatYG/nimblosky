bd922b371e6780d6c6016c19fc9cfa1bafa8f081
KursatYG <kursat.yegin.1907@gmail.com>
2025-05-20 22:29:50 +0300

    update

---
+++ .vite/deps/@iconify_react.js
@@ -0,0 +1,38 @@
+"use client";
+import {
+  Icon,
+  InlineIcon,
+  _api,
+  addAPIProvider,
+  addCollection,
+  addIcon,
+  calculateSize,
+  getIcon,
+  iconLoaded,
+  iconToSVG,
+  listIcons,
+  loadIcon,
+  loadIcons,
+  replaceIDs,
+  setCustomIconLoader,
+  setCustomIconsLoader
+} from "./chunk-56VGSHMG.js";
+import "./chunk-5WQJO2FO.js";
+export {
+  Icon,
+  InlineIcon,
+  _api,
+  addAPIProvider,
+  addCollection,
+  addIcon,
+  iconToSVG as buildIcon,
+  calculateSize,
+  getIcon,
+  iconLoaded,
+  listIcons,
+  loadIcon,
+  loadIcons,
+  replaceIDs,
+  setCustomIconLoader,
+  setCustomIconsLoader
+};
